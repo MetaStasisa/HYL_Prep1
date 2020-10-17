454718f993518493ee387989955a0b584c35f5a0
anandp1 <73033746+anandp1@users.noreply.github.com>
2020-10-17 14:42:06 -0600

    ParkingStall

--- ParkingLotSimulator_py/ParkingStall.py
+++ ParkingLotSimulator_py/ParkingStall.py
@@ -4,16 +4,24 @@ from CarClass import CarClass
 
 class ParkingStall:
     
-    def __init__(self, car = None):
+    def __init__(self, x, y, delta_x, delta_y, car = None):
         self.car = car
-        # self.x = x
-        # self.y = y
-        # self.colour = colour
-    
-    # def DrawCell(self):
-    #     fill(self.colour[0])
-    #     rect(self.x, self.y, 82.5, 65)
-
+        self.x = x
+        self.y = y
+        self.delta_y = delta_y
+        self.delta_x = delta_x
+        #extra
+        
+    def DrawCell(self):
+        if self.car:
+            stroke(255, 255, 255)
+            fill(255, 0, 0)
+            rect(self.x, self.y, self.delta_x, self.delta_y)
+        else:
+            stroke(255, 255, 255)
+            fill(0, 255, 0)
+            rect(self.x, self.y, self.delta_x, self.delta_y)
+        
     def SetCar(self, car):
         self.car = car
         
