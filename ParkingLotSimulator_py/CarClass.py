5eb41b06f3f20fa6b01391596b1bdb3eb5097a28
vanessahychen <60018155+vanessahychen@users.noreply.github.com>
2020-10-17 14:32:42 -0600

    Update CarClass.py

--- ParkingLotSimulator_py/CarClass.py
+++ ParkingLotSimulator_py/CarClass.py
@@ -1,8 +1,8 @@
 import random
 
 class CarClass:
+    # Constructor to set the x and y position of the car, and the colour (RGB) and number of the car. It also chooses randomly if the car is going to be parking
     def __init__(self, car_x, car_y, car_R, car_G, car_B, car_number):
-        
         self.car_x = car_x
         self.car_y = car_y
         self.RGB = [car_R, car_G, car_B]
@@ -10,13 +10,14 @@ class CarClass:
         self.IsParking = random.randint(0, 1)
         self.DrawCar(self)
         
+    # Member function to draw the car according to the RGB colour from the constructor. It also outputs text saying if a car is going to be parking
     def DrawCar(self):
         noStroke()
         fill(self.RGB[0], self.RGB[1], self.RGB[2])
         rect(self.car_x, self.car_y, 75, 50)
         if self.IsParking == 1:
             fill(255, 255, 255)
-            text("PARKING", self.car_x - 10, self.car_y - 10)
+            text("PARKING", self.car_x, self.car_y - 10)
     
     def GetR(self):
         return self.RGB[0]
