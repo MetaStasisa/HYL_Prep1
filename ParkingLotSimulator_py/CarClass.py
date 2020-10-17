178104ac28b3024872e436757fc6724d64f9e6df
anandp1 <73033746+anandp1@users.noreply.github.com>
2020-10-17 13:09:08 -0600

    CarClass

--- ParkingLotSimulator_py/CarClass.py
+++ ParkingLotSimulator_py/CarClass.py
@@ -1,5 +1,11 @@
 class CarClass:
-
-        
+    def __init__(self, car_colour, car_number):
+        self.car_colour = car_colour
+        self.car_number = car_number
+    
+    def GetAttributes(self):
+        return dict_car{"car_colour": f"{self.car_colour}",
+                          "car_numer": self.car_number}
+    
     
     
--- ParkingLotSimulator_py/ParkingStall.py
+++ ParkingLotSimulator_py/ParkingStall.py
@@ -4,9 +4,15 @@ from CarClass import CarClass
 
 class ParkingStall:
     
-    def __init__(self, car = None):
+    def __init__(self, car = None, x, y, colour):
         self.car = car
+        self.x = x
+        self.y = y
+        self.colour = colour
     
+    def DrawCell(self):
+        fill(self.colour[0]
+        rect(self.x, self.y, 82.5, 65)
     def SetCar(self, car):
         self.car = car
         
