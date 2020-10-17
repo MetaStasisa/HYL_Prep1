9253df4113af22cc590ee03167a7d0d04b5384ea
micdean19 <55826264+micdean19@users.noreply.github.com>
2020-10-17 12:47:00 -0600

    Parking Stall Section Class

---
+++ ParkingLotSimulator_py/cParkingStallSection.py
@@ -0,0 +1,63 @@
+import random
+
+class ParkingStallSection:
+    def __init__(self, maximum=8):
+        self.iNumberofcars = 0
+        self.iMaximumcars = maximum
+        self.dParkingStalls = self.initialize()
+        
+    def initialize(self):
+        dTemporaryContainer = dict()
+        for i in range(self.iMaximumcars):
+            strTemp = "stall" + str(i)
+            # Calling Stall Class
+            dTemporaryContainer['strTemp'] = ParkingStall()
+        return dTemporaryContainer
+    
+    def isEmpty(self):
+        if self.iNumberofcars == 0:
+            return True
+        else:
+            return False
+        
+    def isFull(self):
+        if self.iNumberofcars==self.iMaximumcars:
+            return True
+        else:
+            return False
+        
+    def PopCar(self):
+        while True:
+            iTemp = random.randint(0, self.iMaximumcars)
+            sTemp = self.__ConstructKey(iTemp)
+            # If Empty then Move on to next stall
+            if not self.__CheckifStallisUsed(sTemp):
+                continue
+            self.dParkingStalls[sTemp].removeCar()
+            break
+        return False
+            
+    def PushCar(self, Car aCar):
+        while True:
+            iTemp = random.randint(0, self.iMaximumcars)
+            sTemp = self.__ConstructKey(iTemp)
+            # If Full move on to next stall until you get empty
+            if self.__CheckifStallisUsed(sTemp):
+                continue
+            self.dParkingStalls[sTemp].SetCar(aCar)
+            break
+        return True
+    
+    def __ConstructKey(self, stall:int):
+        return "stall" + str(stall)
+    
+    def __CheckifStallisUsed(self, stall):
+        """ Receives a string and calls constructor, Returns true or false if stall is fill """
+        if (self.dParkingStalls[stall].isFull()):
+            return True
+        
+        
+        
+    
+        
+    
